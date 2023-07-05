ecea188d3c6a0632c1d4ced0791c45937566220f
Konstantin <alkapone10031993@gmail.com>
2023-07-05 13:04:34 +0300

    12

---
+++ 4/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+.Сформируйте двумерный массив из неповторяющихся двузначных чисел.
+ Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+Console.WriteLine($"\nInput array X x Y:");
+int x = InputNumbers("Input X: ");
+int y = InputNumbers("Input Y: ");
+Console.WriteLine($"");
+
+int[,] array3D = new int[x, y];
+CreateArray(array3D);
+WriteArray(array3D);
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void WriteArray (int[,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+    Console.Write($"X({i}) Y({j})");
+    Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void CreateArray(int[,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    
+    array3D[x, y] = temp[count];
+    count++;
+      
+    }
+  }
